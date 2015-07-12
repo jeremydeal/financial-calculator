e8096c3908f1ed143ff12a08c0a5b3692318ab41
Jeremy Deal <jeremy.n.deal@gmail.com>
2015-07-12 01:50:53 -0600

    Added unit tests for CalculateFv()

--- UnitTests/CalculatorTest.cs
+++ UnitTests/CalculatorTest.cs
@@ -8,7 +8,7 @@ namespace UnitTests
     public class CalculatorTest
     {
         [TestMethod]
-        [TestCategory("PV")]
+        [TestCategory("Pv")]
         public void CalcPvFromSum()
         {
             Calculator.Initialize();
@@ -22,7 +22,7 @@ namespace UnitTests
         }
 
         [TestMethod]
-        [TestCategory("PV")]
+        [TestCategory("Pv")]
         public void CalcPvFromSumMonthly()
         {
             Calculator.Initialize();
@@ -37,7 +37,7 @@ namespace UnitTests
         }
 
         [TestMethod]
-        [TestCategory("PV")]
+        [TestCategory("Pv")]
         public void CalcPvFromAnnuity()
         {
             Calculator.Initialize();
@@ -51,7 +51,7 @@ namespace UnitTests
         }
 
         [TestMethod]
-        [TestCategory("PV")]
+        [TestCategory("Pv")]
         public void CalcPvFromAnnuityMonthly()
         {
             Calculator.Initialize();
@@ -64,6 +64,65 @@ namespace UnitTests
 
             Assert.AreEqual(9007.35m, Math.Round(Calculator.Pv, 2));
         }
+
+        [TestMethod]
+        [TestCategory("Fv")]
+        public void CalcFvFromSum()
+        {
+            Calculator.Initialize();
+            Calculator.Pv = -10000;
+            Calculator.Rate = 6;
+            Calculator.Nper = 10;
+
+            Calculator.CalculateFv();
+
+            Assert.AreEqual(17908.48m, Math.Round(Calculator.Fv, 2));
+        }
+
+        [TestMethod]
+        [TestCategory("Fv")]
+        public void CalcFvFromSumMonthly()
+        {
+            Calculator.Initialize();
+            Calculator.CompoundingPeriods = 12;
+            Calculator.Pv = -10000;
+            Calculator.Rate = 6;
+            Calculator.Nper = 10;
+
+            Calculator.CalculateFv();
+
+            Assert.AreEqual(18193.97m, Math.Round(Calculator.Fv, 2));
+        }
+
+        [TestMethod]
+        [TestCategory("Fv")]
+        public void CalcFvFromAnnuity()
+        {
+            Calculator.Initialize();
+            Calculator.Rate = 6;
+            Calculator.Nper = 10;
+            Calculator.Pmt = -1000;
+
+            Calculator.CalculateFv();
+
+            Assert.AreEqual(13180.79m, Math.Round(Calculator.Fv, 2));
+        }
+
+        [TestMethod]
+        [TestCategory("Fv")]
+        public void CalcFvFromAnnuityMonthly()
+        {
+            Calculator.Initialize();
+            Calculator.CompoundingPeriods = 12;
+            Calculator.Rate = 6;
+            Calculator.Nper = 10;
+            Calculator.Pmt = -100;
+
+            Calculator.CalculateFv();
+
+            Assert.AreEqual(16378.93m, Math.Round(Calculator.Fv, 2));
+        }
+
 
     }
 }
